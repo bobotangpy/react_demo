bd8189bf22379f4a62d1c70ba88e733ea3682887
Bobo Tang <bobotangpy@gmail.com>
2020-08-07 15:36:32 +0800

    made changes to knexfile staging & production info

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -15,32 +15,40 @@ module.exports = {
   staging: {
     client: "postgresql",
     connection: {
-      database: process.env.DB_DATABASE,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD
+      database: process.env.DATABASE_URL,
+      // user: process.env.DB_USERNAME,
+      // password: process.env.DB_PASSWORD
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: "knex_migrations"
-    }
+      directory: "./migrations/20200806183232_create-tables"
+    },
+    seeds: {
+      directory: './seeds',
+    },
+    ssl: true
   },
 
   production: {
     client: "postgresql",
     connection: {
-      database: process.env.DB_DATABASE,
-      user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD
+      database: process.env.DATABASE_URL,
+      // user: process.env.DB_USERNAME,
+      // password: process.env.DB_PASSWORD
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: "knex_migrations"
-    }
+      directory: "./migrations/20200806183232_create-tables"
+    },
+    seeds: {
+      directory: './seeds',
+    },
+    ssl: true
   }
 };
