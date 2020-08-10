3bd754eee70cbfcd641253e11c61d3ce297435df
Bobo Tang <bobotangpy@gmail.com>
2020-08-10 18:39:43 +0800

    silly mistake

--- backend/app.js
+++ backend/app.js
@@ -109,7 +109,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../zora_react/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'zora_react', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../zora_react', 'build', 'index.html'));
   });
 }
 
