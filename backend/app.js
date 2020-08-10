64f4d936a9a496a2332f6f4c7e52d26064db2b94
Bobo Tang <bobotangpy@gmail.com>
2020-08-10 19:02:41 +0800

    T_T

--- backend/app.js
+++ backend/app.js
@@ -109,7 +109,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../zora_react/build')));
 
   app.get('*', (req, res) => {
-    let url = path.join(__dirname, '../zora_react', 'build', 'index.html');
+    let url = path.join(__dirname, '../zora_react', 'build');
     if (!url.startsWith('/app/')) // since we're on local windows
       url = url.substring(1);
     res.sendFile(url);
