e2a7f727b12b3b4ba15eecc2610a89b1d51aa693
Bobo Tang <bobotangpy@gmail.com>
2020-08-10 21:15:19 +0800

    rm /build frm gitignore

---
+++ zora_react/build/precache-manifest.1501743c2413cf921a186376d40c5b71.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d29e734ecad28af17c21c038c2a76be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0642312892af8b81f4d",
+    "url": "/static/css/2.b0925b0c.chunk.css"
+  },
+  {
+    "revision": "0600381c2324e5f75980",
+    "url": "/static/css/main.9896373b.chunk.css"
+  },
+  {
+    "revision": "d0642312892af8b81f4d",
+    "url": "/static/js/2.39967249.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.39967249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0600381c2324e5f75980",
+    "url": "/static/js/main.00613734.chunk.js"
+  },
+  {
+    "revision": "289ef1ccaa37a5be61eb",
+    "url": "/static/js/runtime-main.aca1a401.js"
+  },
+  {
+    "revision": "6522aac8d52fab3bbc5d515dd26d0465",
+    "url": "/static/media/Aquarius.6522aac8.png"
+  },
+  {
+    "revision": "fe06eff9792b951fe79eaa82b054b779",
+    "url": "/static/media/Aquarius_bg.fe06eff9.png"
+  },
+  {
+    "revision": "ff6ddcb0893d4273bbf7f7f1e3e87155",
+    "url": "/static/media/Aries.ff6ddcb0.png"
+  },
+  {
+    "revision": "4ce6c27871e0ccb2fac24129d2d8c5f6",
+    "url": "/static/media/Aries_bg.4ce6c278.png"
+  },
+  {
+    "revision": "d69c108b1c94b50fc9213a6797e256d9",
+    "url": "/static/media/Cancer.d69c108b.png"
+  },
+  {
+    "revision": "05b7d391ea45c60345e05f349dcf4fc9",
+    "url": "/static/media/Cancer_bg.05b7d391.png"
+  },
+  {
+    "revision": "f15f6da0496d390cb8418e99671986e7",
+    "url": "/static/media/Capricorn.f15f6da0.png"
+  },
+  {
+    "revision": "1c9052ae84601b370d951644ff23fdc3",
+    "url": "/static/media/Capricorn_bg.1c9052ae.png"
+  },
+  {
+    "revision": "fe09e45c5249f125ab1e5256aa6814ab",
+    "url": "/static/media/Leo.fe09e45c.png"
+  },
+  {
+    "revision": "7ca9ea837d587f38fd7e063c3cbfef88",
+    "url": "/static/media/Leo_bg.7ca9ea83.png"
+  },
+  {
+    "revision": "9411606493872b7e221305c9367eea82",
+    "url": "/static/media/Libra_bg.94116064.png"
+  },
+  {
+    "revision": "10cc1d593cf8b5e15b39042061ea0483",
+    "url": "/static/media/Pisces.10cc1d59.png"
+  },
+  {
+    "revision": "5a1cfc179ecaec713f666e857147e6a9",
+    "url": "/static/media/Pisces_bg.5a1cfc17.png"
+  },
+  {
+    "revision": "73af3cc97c9dff94b0a730a9b65fdb90",
+    "url": "/static/media/Sagittarius.73af3cc9.png"
+  },
+  {
+    "revision": "cf39dea4e85007e78dd9d1e7c7c7d600",
+    "url": "/static/media/Sagittarius_bg.cf39dea4.png"
+  },
+  {
+    "revision": "80c6d16ef5de5af60fe5ffacbcd4d2e7",
+    "url": "/static/media/Scorpio.80c6d16e.png"
+  },
+  {
+    "revision": "d89909e44ae4a6fe2f31ced13b5b49e8",
+    "url": "/static/media/Scorpio_bg.d89909e4.png"
+  },
+  {
+    "revision": "a5c6264ef195f928218d090cb74a97b9",
+    "url": "/static/media/Taurus_bg.a5c6264e.png"
+  },
+  {
+    "revision": "3898c45d16c51c89c80c6f7691fec5c2",
+    "url": "/static/media/Virgo.3898c45d.png"
+  },
+  {
+    "revision": "686ec206973a31489c0909fd1ef9154b",
+    "url": "/static/media/Virgo_bg.686ec206.png"
+  },
+  {
+    "revision": "d0b3a1d09c1a1218e0ab8914c1052e37",
+    "url": "/static/media/cart.d0b3a1d0.png"
+  },
+  {
+    "revision": "88aa067763a22f8d2c40ba5ef6d6174f",
+    "url": "/static/media/horoscope_bg.88aa0677.jpg"
+  },
+  {
+    "revision": "38dd300919aa4b4534b9bd66f36c9dfc",
+    "url": "/static/media/landing_bg.38dd3009.jpg"
+  },
+  {
+    "revision": "59e71426864d96a5db9e0ccfc2537a2c",
+    "url": "/static/media/logo.59e71426.png"
+  },
+  {
+    "revision": "8e68a8d209ff0edc246d4a62e9881ec8",
+    "url": "/static/media/shop_men.8e68a8d2.jpg"
+  },
+  {
+    "revision": "e5d0b772c6c659cb2a18b7190e855001",
+    "url": "/static/media/shop_women.e5d0b772.jpg"
+  },
+  {
+    "revision": "38ff7a251d9c17d21b69fd3d15ebbeb7",
+    "url": "/static/media/slide_1.38ff7a25.jpg"
+  },
+  {
+    "revision": "93ec6b116dc1a1a80df0d2c3215853f8",
+    "url": "/static/media/slide_2.93ec6b11.jpg"
+  },
+  {
+    "revision": "d9b20003479adccc81e7569925150c62",
+    "url": "/static/media/slide_3.d9b20003.jpg"
+  }
+]);
