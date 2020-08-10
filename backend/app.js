6233a694102db9313fa8a8b96abb3d1095ccd088
Bobo Tang <bobotangpy@gmail.com>
2020-08-10 21:37:47 +0800

    fixing backend connection

--- backend/app.js
+++ backend/app.js
@@ -116,6 +116,16 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
+const pg = require('pg');
+const connectionString = "postgres://clqhawoxctrebo:8fd784ec39f84f33d188eb476a9171c75a44d13d20c09e1bb9078781750deeea@ec2-54-146-91-153.compute-1.amazonaws.com:5432/d1443rh6ogu243"
+pg.connect(connectionString, function(err, client, done) {
+  client.query('SELECT * FROM your_table', function(err, result) {
+     done();
+     if(err) return console.error(err);
+     console.log(result.rows);
+  });
+});
+
 
 const port = process.env.PORT || 8880;
 app.listen(port, () => {
