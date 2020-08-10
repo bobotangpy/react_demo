c6ec5c38656b0c8278eb6fea2837ddeac618295e
Bobo Tang <bobotangpy@gmail.com>
2020-08-10 18:12:32 +0800

    try try

--- backend/app.js
+++ backend/app.js
@@ -107,9 +107,9 @@ app.use("/api/orderHistory", orderHistoryRoute.router());
 // Added for Heroku
 app.use(express.static(path.join(__dirname, '../zora_react/public')));
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, './zora_react/public'));
-});
+// app.get('*', function(req, res) {
+//   res.sendFile(path.join(__dirname, './zora_react/public'));
+// });
 
 
 const port = process.env.PORT || 8880;
