792d12abc6895bd6e076e7633e4fcc96ec157aef
Bobo Tang <bobo.tang@prizm.digital>
2020-11-12 15:24:19 +0800

    try rm ${process.env.REACT_APP_API_SERVER}

--- zora_react/src/redux/productsList/actions.js
+++ zora_react/src/redux/productsList/actions.js
@@ -9,55 +9,57 @@ import axios from "axios";
 //     withCredentials: true,
 // });
 
-export const LOAD_ITEMS_SUCCESS = "LOAD_ITEMS_SUCCESS"
-export const LOAD_ITEMS_FAILURE = "LOAD_ITEMS_FAILURE"
+export const LOAD_ITEMS_SUCCESS = "LOAD_ITEMS_SUCCESS";
+export const LOAD_ITEMS_FAILURE = "LOAD_ITEMS_FAILURE";
 
 export function loadItemsSuccess(responseData) {
-    return {
-        type: LOAD_ITEMS_SUCCESS,
-        items: responseData
-    }
+  return {
+    type: LOAD_ITEMS_SUCCESS,
+    items: responseData,
+  };
 }
 
 export function loadItemsFailure(message) {
-    return {
-        type: LOAD_ITEMS_FAILURE,
-        message: message
-    }
+  return {
+    type: LOAD_ITEMS_FAILURE,
+    message: message,
+  };
 }
 
 export function getHoroscopeItems(horoscope, style) {
-    return (dispatch) => {
-        return axios.get(`${process.env.REACT_APP_API_SERVER}/api/clothes/highlights/${horoscope}/${style}`, {
-        })
-        .then(response => {
-            // console.log(response)
-            if (response.data == null) {
-                dispatch(loadItemsFailure('No response.'))
-            } else {
-                // console.log(response.data)
-                dispatch(loadItemsSuccess(response.data))
-            }
-        }).catch((e) => {
-            console.log('Cannot get items. ' + e)
-        })
-    }
+  return (dispatch) => {
+    return axios
+      .get(`/api/clothes/highlights/${horoscope}/${style}`, {})
+      .then((response) => {
+        // console.log(response)
+        if (response.data == null) {
+          dispatch(loadItemsFailure("No response."));
+        } else {
+          // console.log(response.data)
+          dispatch(loadItemsSuccess(response.data));
+        }
+      })
+      .catch((e) => {
+        console.log("Cannot get items. " + e);
+      });
+  };
 }
 
 export function getGenderItems(gender, style, type) {
-    return (dispatch) => {
-        return axios.get(`${process.env.REACT_APP_API_SERVER}/api/clothes/${gender}/${style}/${type}`, {
-        })
-        .then(response => {
-            // console.log(response)
-            if (response.data == null) {
-                dispatch(loadItemsFailure('No response.'))
-            } else {
-                // console.log(response.data)
-                dispatch(loadItemsSuccess(response.data))
-            }
-        }).catch((e) => {
-            console.log('Cannot get items. ' + e)
-        })
-    }
+  return (dispatch) => {
+    return axios
+      .get(`/api/clothes/${gender}/${style}/${type}`, {})
+      .then((response) => {
+        // console.log(response)
+        if (response.data == null) {
+          dispatch(loadItemsFailure("No response."));
+        } else {
+          // console.log(response.data)
+          dispatch(loadItemsSuccess(response.data));
+        }
+      })
+      .catch((e) => {
+        console.log("Cannot get items. " + e);
+      });
+  };
 }
